1e59a58c7e28b531fd9284ec74c599c0fa7b9598
ruveyharuzgar <ruveyharuzgar.108@gmail.com>
2021-02-03 23:55:05 +0300

    first commit

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class:referans tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new(): new'lenebilir olmalı (IEntity newlenemiyodu ya)
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        //burda productManager de ki getall ı çağırıyorduk ya, onun içerisine
+        //istediğim filtreyi ayrı ayrı yazmamak için böyle yazıyorum.HEpsini görüntüle
+        //aşağıda ki gibi de yazarsam filtre verdiysem öyle yaz
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        //List<T> GetAllByCategory(int categoryId);
+    }
+}
